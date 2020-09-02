7fdbda9b4684bbf11e663860d369b56796a65ddc
Andrea <miranda.andrea.nd@gmail.com>
2020-09-02 16:45:51 +0200

    snack-1-

---
+++ index.php
@@ -0,0 +1,25 @@
+<!-- Creiamo un array contenente le partite di basket di un'ipotetica tappa del calendario.
+
+Ogni array avrà una squadra di casa e una squadra ospite,
+punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
+Stampiamo a schermo tutte le partite con questo schema. -->
+
+
+
+
+<?php
+
+$partite = [
+
+  "10/09/20" => ["Torino" => "55" , "Milano"=> "60"] ,
+  "13/09/20" => ["Firenze" => "50" , "Bologna"=> "45"], 
+
+];
+
+
+
+
+
+
+ var_dump ($partite);
+ ?>
